2cde97e98121a9af0aa7739e1467b6fcd1a7730b
Dmitry <dmtKlenye@gmail.com>
2018-05-01 22:55:59 +0300

    Initial Commit

--- GitReader/Program.cs
+++ GitReader/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace GitReader
 {
@@ -10,6 +12,54 @@ namespace GitReader
     {
         static void Main(string[] args)
         {
+            var pathToRepository = @"F:\GitHub\suvoda\suvoda-services.IRT\";
+            var studyList = new List<StudyInfo>(); 
+
+            using (var repo = new Repository(pathToRepository))
+            {
+                var remoteBranches = repo.Branches.Where(x => x.IsRemote).ToList();
+
+                remoteBranches.ForEach(branch =>
+                {
+
+                    Commands.Checkout(repo, branch);
+
+                    var study = new StudyInfo
+                    {
+                        StudyName = branch.FriendlyName
+                    };
+
+                    Console.WriteLine(study.StudyName);
+
+                    var modules = new List<string>();
+                    var regex = new Regex(".*suvoda[.]irt[.]modules.*[.]dll", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+
+                    repo.Index.ToList().Where(x => regex.IsMatch(x.Path)).ToList().ForEach(x =>
+                    {
+                        var moduleName = GetModuleName(x.Path);
+
+                        if (!modules.Contains(moduleName))
+                        {
+                            modules.Add(moduleName);
+                        }
+                    });
+
+                    modules.ForEach(x => Console.WriteLine("\t" + x));
+
+                    study.Modules = modules;
+                    studyList.Add(study);
+
+
+                });
+
+                File.WriteAllText("studyInfo.json", JsonConvert.SerializeObject(studyList,Formatting.Indented));
+            }
+        }
+
+        static string GetModuleName(string path)
+        {
+            return path.Split('/').Last().Replace(".dll", "").Replace("Suvoda.IRT.Modules.", "");
         }
     }
 }
---
+++ GitReader/StudyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitReader
+{
+    [Serializable]
+    public class StudyInfo
+    {
+        public string StudyName { get; set; }
+        public IEnumerable<string> Modules { get; set; }
+    }
+}
